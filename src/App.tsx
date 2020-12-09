f85a074eecd8146223b436ecc897e07e5018191e
sixiaofeng <hzsixiaofeng@highstore.cn>
2020-12-09 09:44:04 +0800

    项目初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import styled from "styled-components"
+import Nav from "./components/Nav"
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Route exact path="/" component={Money} />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav />
+      </Wrapper>
+    </Router>
   );
 }
 
-export default App;
+function Statistics() {
+  return <h2>统计页</h2>;
+}
+
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页</h2>;
+}
+
+function NoMatch() {
+  return <h2>404</h2>
+}
+
+export default App
