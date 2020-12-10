9ffe7ac6494b0dc140783078c59d5b8be252e57b
sixiaofeng <hzsixiaofeng@highstore.cn>
2020-12-10 14:02:51 +0800

    标签页样式完成

--- src/views/Money/SectionTags.tsx
+++ src/views/Money/SectionTags.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
-import React, {useState} from "react"
+import React from "react"
+import {useTags} from "../../useTags"
 
 const Wrapper = styled.section`
  background: #fff;
@@ -37,7 +38,8 @@ type Props = {
   onChange: (selected: string[]) => void
 }
 const SectionTags: React.FC<Props> = (props) => {
-  const [tags, setTags] = useState<string[]>(['衣','食','住','行'])
+  // const [tags, setTags] = useState<string[]>(['衣','食','住','行'])
+  const {tags, setTags} = useTags()
   const selectedTags = props.value
   const onAddTag = () => {
     let tagName = window.prompt('请输入新标签的名称')
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,10 +1,55 @@
 import Layout from "../components/Layout"
 import React from "react"
+import {useTags} from "useTags"
+import styled from "styled-components"
+import Icon from "../components/Icon"
 
+const TagList = styled.ul`
+  font-size: 16px;
+  background: #fff;
+  > li {
+    border-bottom: 1px solid #ddd;
+    padding: 12px 16px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    > .icon {
+       height: 20px;
+       width: 20px;
+       flex-shrink: 0;
+    }
+  }
+`
+const Button = styled.button`
+  font-size: 14px;
+  padding: 8px 18px;
+  background: #ddd;
+  border: none;
+  margin: auto;
+  border-radius: 4px;
+`
+const CenterBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 24px 0;
+`
 function Tags() {
+  const {tags, setTags} = useTags()
   return (
     <Layout>
-      <h2>标签页</h2>
+      <TagList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <span className="one-line">{tag}</span>
+            <Icon name="arrow-right" />
+          </li>
+        )}
+      </TagList>
+      <CenterBox>
+        <Button>新增标签</Button>
+      </CenterBox>
     </Layout>
   )
 }
