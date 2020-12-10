134ab3e8ea720097c7f8d2b943f1ad1432bce57f
sixiaofeng <hzsixiaofeng@highstore.cn>
2020-12-10 09:55:43 +0800

    数字输入功能完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -17,25 +17,7 @@ function Money() {
       <SectionTags />
       <SectionNote />
       <SectionCategory />
-      <SectionNumber>
-        <div className="output">100</div>
-        <div className="pad clearFix">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>删除</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>清空</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className="ok">OK</button>
-          <button className='zero'>0</button>
-          <button>.</button>
-        </div>
-      </SectionNumber>
+      <SectionNumber />
     </MyLayout>
   )
 }
--- src/views/Money/SectionNumber.tsx
+++ src/views/Money/SectionNumber.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components"
+import React, {useState} from "react"
+import {calculateOutput} from "./sectionNumber/calculateOutput"
 
-const SectionNumber = styled.section`
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   > .output {
@@ -18,7 +20,6 @@ const SectionNumber = styled.section`
       width: 25%;
       height: 64px;
       float: left;
-      border: none;
       border: 1px solid #ddd;
       &.ok {
         float: right;
@@ -30,5 +31,48 @@ const SectionNumber = styled.section`
     }
   }
 `
+const SectionNumber: React.FC = () => {
+  const [output, _setOutput] = useState<string>('0')
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16)
+    } else if (output.length === 0) {
+      output = '0'
+    }
+    _setOutput(output)
+  }
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent
+    if (text === null) return
+    if (text === 'OK') {
+      console.log('提交')
+      return
+    }
+    if ('1234567890.'.split('').concat(['删除', '清空']).indexOf(text) > -1) {
+      setOutput(calculateOutput(text, output))
+    }
+  }
+  return (
+    <Wrapper>
+      <div className="output">{output}</div>
+      <div onClick={(e) => onClickButtonWrapper(e)} className="pad clearFix">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="ok">OK</button>
+        <button className='zero'>0</button>
+        <button>.</button>
+      </div>
+    </Wrapper>
+  )
+}
 
 export {SectionNumber}
